bf586c26121c19c60c79eb2d02fb0a5dd9dc9bd1
Golovitskiy Mikhail <m.golovitskiy@gmail.com>
2020-10-05 23:24:41 +0300

    Homework #1 (free one)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,70 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int storageSize = 0;
 
     void clear() {
+        for (int i = 0; i < storageSize; i++) {
+            storage[i] = null;
+        }
+        storageSize = 0;
     }
 
     void save(Resume r) {
+        boolean error = false;
+        if (storageSize == storage.length) {
+            System.out.println("The array size would be exceeded.");
+            error = true;
+        }
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                System.out.println("Resume with such uuid already exists.");
+                error = true;
+            }
+        }
+        if (!error) {
+            storage[storageSize] = r;
+            storageSize++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        if (storageSize == 0) {
+            System.out.println("Storage is already empty.");
+        } else {
+            int oldSize = storageSize;
+            for (int i = 0; i < storageSize; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    System.arraycopy(storage, i + 1, storage, i, storageSize - i - 1);
+                    storageSize--;
+                }
+            }
+            if (oldSize == storageSize) {
+                System.out.println("No such resume in storage.");
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[storageSize];
+        System.arraycopy(storage, 0, resumes, 0, storageSize);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return storageSize;
     }
 }
