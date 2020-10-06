699963b1150ea98ba26548a90c9fc18c2891f364
Golovitskiy Mikhail <m.golovitskiy@gmail.com>
2020-10-06 22:40:26 +0300

    Homework #1 (after code review)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -13,18 +13,18 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        boolean error = false;
+        int index = -1;
         if (storageSize == storage.length) {
             System.out.println("The array size would be exceeded.");
-            error = true;
+            return;
         }
         for (int i = 0; i < storageSize; i++) {
             if (storage[i].uuid.equals(r.uuid)) {
                 System.out.println("Resume with such uuid already exists.");
-                error = true;
+                index = i;
             }
         }
-        if (!error) {
+        if (index != -1) {
             storage[storageSize] = r;
             storageSize++;
         }
@@ -40,19 +40,17 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        if (storageSize == 0) {
-            System.out.println("Storage is already empty.");
-        } else {
-            int oldSize = storageSize;
-            for (int i = 0; i < storageSize; i++) {
-                if (storage[i].uuid.equals(uuid)) {
-                    System.arraycopy(storage, i + 1, storage, i, storageSize - i - 1);
-                    storageSize--;
-                }
+        int index = -1;
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                System.arraycopy(storage, i + 1, storage, i, storageSize - i - 1);
+                storageSize--;
+                break;
             }
-            if (oldSize == storageSize) {
-                System.out.println("No such resume in storage.");
-            }
+        }
+        if (index == -1) {
+            System.out.println("No such resume in storage.");
         }
     }
 
