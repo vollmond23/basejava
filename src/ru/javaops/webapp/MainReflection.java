e9b19f6d1209806635bde2d04e5afd8374da7c47
Golovitskiy Mikhail <m.golovitskiy@gmail.com>
2020-10-26 22:49:56 +0300

    Homework #4 (correction after watching HW4 analysis)

--- src/ru/javaops/webapp/MainReflection.java
+++ src/ru/javaops/webapp/MainReflection.java
@@ -9,14 +9,15 @@ import java.lang.reflect.Method;
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
         Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Class<? extends Resume> resumeClass = r.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println("Field name: " + field.getName());
         System.out.println("Field value: " + field.get(r));
         field.set(r, "new_uuid");
         System.out.println("r.toString: " + r);
 
-        Method method = r.getClass().getDeclaredMethod("toString");
+        Method method = resumeClass.getDeclaredMethod("toString");
         System.out.print("r.toString via reflection: ");
         method.setAccessible(true);
         System.out.println(method.invoke(r));
