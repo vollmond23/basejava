ae069f669221d3332b87e5abcdcf28f119654e45
Golovitskiy Mikhail <m.golovitskiy@gmail.com>
2021-01-16 14:18:28 +0300

    Homework #11 (code review #1)

--- src/ru/javaops/webapp/Deadlock.java
+++ src/ru/javaops/webapp/Deadlock.java
@@ -1,41 +1,30 @@
 package ru.javaops.webapp;
 
 public class Deadlock {
-    private static final int MILLIS = 100;
     private static final String STRING_1 = "string1";
     private static final String STRING_2 = "string2";
 
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread(() -> {
-            synchronized (STRING_1) {
-                System.out.println("Thread1 locked string1");
-                try {
-                    Thread.sleep(MILLIS);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (STRING_2) {
-                    System.out.println("Thread1 locked string2");
-                }
-            }
-        });
+        lockThread(STRING_1, STRING_2);
+        lockThread(STRING_2, STRING_1);
+    }
 
-        Thread thread2 = new Thread(() -> {
-            synchronized (STRING_2) {
-                System.out.println("Thread2 locked string2");
-                try {
-                    Thread.sleep(MILLIS);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (STRING_1) {
-                    System.out.println("Thread2 locked string1");
-                }
-            }
-        });
-
-        thread1.start();
-        thread2.start();
+    private static <T> void lockThread(T lock1, T lock2) {
+        Thread thread = new Thread(() -> {
+                   synchronized (lock1) {
+                       String threadName = Thread.currentThread().getName();
+                       System.out.println(threadName + " locked " + lock1);
+                       try {
+                           Thread.sleep(100);
+                       } catch (InterruptedException e) {
+                           e.printStackTrace();
+                       }
+                       synchronized (lock2) {
+                           System.out.println(threadName + " locked " + lock2);
+                       }
+                   }
+                });
+        thread.start();
     }
 }
