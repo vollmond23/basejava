6f2afcd74f4e4790c96d7bed635e0ac53b66bdcb
Golovitskiy Mikhail <m.golovitskiy@gmail.com>
2021-02-08 11:40:29 +0300

    Homework #15

--- config/init_db.sql
+++ config/init_db.sql
@@ -29,21 +29,21 @@ alter table contact
 create unique index contact_uuid_type_index
     on contact (resume_uuid, type);
 
-create table text_section
+create table section
 (
     id serial not null
-        constraint text_section_pk
+        constraint section_pk
             primary key,
     type text not null,
     content text not null,
     resume_uuid char(36) not null
-        constraint text_section_resume_uuid_fk
+        constraint section_resume_uuid_fk
             references resume
             on delete cascade
 );
 
 alter table section owner to postgres;
 
-create unique index text_section_uuid_type_index
+create unique index section_uuid_type_index
     on section (resume_uuid, type);
 
